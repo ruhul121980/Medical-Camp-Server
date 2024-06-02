0189ca3851c3ae11cac198edea08bc1d2a2afd3e
unknown <ruhul121980@gmail.com>
2024-06-02 22:09:50 +0600

    details api added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -41,6 +41,12 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/addCampData/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query = {_id: new ObjectId(id) };
+      const result = await addCamp.findOne(query);
+      res.send(result);
+    })  
 
 
 
