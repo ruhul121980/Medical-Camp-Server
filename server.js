92362d503ef343a661ddd3be67f42a08a0ed7065
unknown <ruhul121980@gmail.com>
2024-06-12 00:53:13 +0600

    hide credentials

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const bodyParser = require('body-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-//i have to put it env file later
-const stripe=require('stripe')('sk_test_51PPO2h04OyLT3Gzbs41SuW41uVr3ctJoK58DpaS2ks02hoxOY8e4TLqo0N6p8OwcmOZB9gkbXqfUSXsWXow4kld800xYZ6kJ9L')
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-const uri = "mongodb+srv://Medi-Camp:D7yBt1PomnbYcNAP@cluster0.v1zto12.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v1zto12.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
